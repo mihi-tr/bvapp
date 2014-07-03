b8d8c9aba0ee06435904cfd1a55b3f355a08b562
Michael Bauer <mihi@lo-res.org>
2014-07-03 10:32:02 +0200

    made scrolling to reload work.

--- backend/static/js/controller.js
+++ backend/static/js/controller.js
@@ -5,11 +5,22 @@ var badVoltageApp = angular.module("badVoltageApp", []).config(
             'http://audio.lugradio.org/badvoltage/**']);
             });
 
+badVoltageApp.directive("scroll", function($window) {
+    return function(scope,element, attrs) {
+        angular.element($window).bind("scroll", function() {
+            if (window.pageYOffset == 0) {
+                scope.load(); };
+            scope.$apply();
+            });
+        }
+    });
 
-badVoltageApp.controller("itemListCtrl" , function($scope,$http) {    
+badVoltageApp.controller("itemListCtrl" , function($scope,$http,$window) {    
     $scope.episodes=[];
-    $http.get('/feed/').success(function(data) {
-        $scope.episodes=data;
-        });
-
+    $scope.load=function() {
+        $http.get('/feed/').success(function(data) {
+            $scope.episodes=data;
+            });
+            };
+    $scope.load();
     });
